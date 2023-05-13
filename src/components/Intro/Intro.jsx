f368bbbe5a59da349b1e8f78ba062c383c6806d9
Ilyos Xolmamtov <ilyosxolmamatov3@gmail.com>
2023-05-13 18:07:01 +0500

    intro content push

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,4 +1,5 @@
 import { Box, Button, IconButton, Typography } from "@mui/material";
+import PlayCircleIcon from '@mui/icons-material/PlayCircle';
 
 export function Intro() {
     return (
@@ -12,12 +13,13 @@ export function Intro() {
                 <Typography variant="subtitle1">
                     Online education is a form of education where students use their home computers through the internet. For many nontraditional students, among them all those who want to continue.
                 </Typography>
-                <Box>
+                <Box display={"flex"} gap={"25px"}>
                     <Button variant="contained" color="success">All Courses</Button>
-                    <IconButton aria-label="play" color="primary" size="medium">
-                        {/* <PlayArrowIcon /> */}
-                        Play Video
+                    <IconButton aria-label="play" color="" size="medium" style={{ background: "white" }}>
+                        <PlayCircleIcon />
+
                     </IconButton>
+                    <Typography variant="h5">  Play Video</Typography>
                 </Box>
             </Box>
             <Box></Box>
